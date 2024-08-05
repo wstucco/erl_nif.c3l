1f0889c362cf3c5cc0f72ccdd2f89dddea77618f
Massimo Ronca <massimo.ronca@leitha.eu>
2024-08-05 16:20:02 +0200

    implement new apis. refactor Makefile

--- c3_erl_nif.ex
+++ c3_erl_nif.ex
@@ -12,8 +12,16 @@ defmodule C3ErlNif do
   end
 
   def hello, do: exit(:nif_not_loaded)
+  def atom, do: exit(:nif_not_loaded)
+  def tuple, do: exit(:nif_not_loaded)
+  def tuple2, do: exit(:nif_not_loaded)
+  def list, do: exit(:nif_not_loaded)
   def add(_a, _b), do: exit(:nif_not_loaded)
 end
 
 IO.puts("nif#add(1+22) = #{C3ErlNif.add(1, 22)}")
 IO.puts("nif#hello() = #{C3ErlNif.hello()}")
+IO.puts("nif#atom() = #{C3ErlNif.atom()}")
+IO.puts("nif#tuple() = #{inspect(C3ErlNif.tuple())}")
+IO.puts("nif#tuple2() = #{inspect(C3ErlNif.tuple2())}")
+IO.puts("nif#list() = #{inspect(C3ErlNif.list())}")
